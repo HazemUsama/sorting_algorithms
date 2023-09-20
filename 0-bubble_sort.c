4b97dad761d8f0025611769de592ecd06014c135
HazemUsama <hazemosama681@gmail.com>
2023-09-21 00:24:23 +0300

    initial commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort the array
+ *
+ * @array: the array
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size - i; j++)
+		if (array[j - 1] > array[j])
+		{
+			tmp = array[j];
+			array[j] = array[j - 1];
+			array[j - 1] = tmp;
+			print_array(array, size);
+		}
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr_node, *prev_node;
+	size_t cnt;
+
+	curr_node = *list;
+	while (curr_node)
+	{
+		cnt = 0;
+		prev_node = curr_node->prev;
+		while (prev_node && curr_node->n < prev_node->n)
+		{
+			curr_node->prev = prev_node->prev;
+			prev_node->next = curr_node->next;
+			if (curr_node->next)
+				curr_node->next->prev = prev_node;
+
+			curr_node->next = prev_node;
+			if (prev_node->prev)
+				prev_node->prev->next = curr_node;
+			else 
+				*list = curr_node;	
+
+			prev_node->prev = curr_node;
+			prev_node = curr_node->prev;
+			cnt++;
+			print_list(*list);
+		}
+
+		while (cnt--)
+			curr_node = curr_node->next;
+
+		curr_node = curr_node->next;
+	}
+}
